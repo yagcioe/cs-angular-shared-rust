39ae632f6802f3e2d6fbac7bcc6fb877daf4f324
Ömer Yagci <yagcioe@gmail.com>
2024-10-02 20:15:38 +0200

    simplify example

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,6 +3,8 @@ using uniffi.dotnet_uniffi;
 while (true)
 {
   Console.WriteLine("Enter string to validate...");
-  var str = Console.ReadLine()??"";
+  var str = Console.ReadLine();
+  if (str == String.Empty) str = null;
+  Console.WriteLine($"entered: {str??"null"}");
   Console.WriteLine($"valid: {DotnetUniffiMethods.Valid(str)}");
 }
