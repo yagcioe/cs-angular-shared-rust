7d8dac23340ffeb9850e7cf0ddb3723f9ed96acf
Ömer Yagci <yagcioe@gmail.com>
2024-10-02 18:54:04 +0200

    add uniffi

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,52 +1,10 @@
-using CsBindgen;
-var str = "a";
-// See https://aka.ms/new-console-template for more information
-  var t = new TestStruct
-  {
-    key = 11,
-    value = 1,
-    value1 = 2,
-    value2 = 3,
-    value3 = 4,
-    value4 = 5,
-    value5 = 6,
-    value6 = 7,
-    value7 = 8,
-    value8 = 9,
-    value9 = 10,
-    value10 = 11,
-    value11 = 12,
-    value12 = 13,
-    value13 = 14,
-    value14 = 15,
-    value15 = 16,
-    value16 = 17,
-    value17 = 18,
-    value18 = 19,
-  };
-var arr = new List<TestStruct> { t }.ToArray();
-double expecetdBytes = 0;
-double mag = 100;
-while (true)
+Console.WriteLine(uniffi.dotnet_uniffi.DotnetUniffiMethods.Test(12));
+Console.WriteLine(uniffi.dotnet_uniffi.DotnetUniffiMethods.TestStr("hello"));
+try
 {
 
-  unsafe
-  {
-    str = NativeMethods.get_csharp_string(str.ToCSharpString()).moveValue();
-    NativeMethods.get_test_mut_arr(arr.ToStructArray()).toArray();
-    t = NativeMethods.get_test_mut(t);
-    expecetdBytes += 80;
-  }
-
-  if(expecetdBytes >= mag)
-  {
-    Console.WriteLine(expecetdBytes/(1024*1024));
-    mag = mag * 10;
-    Console.WriteLine(arr.ToArray()[0].value1);
-  }
-
-  //Console.WriteLine(t.key);
-  //Console.WriteLine(str);
-  //Console.WriteLine(arr[0].key);
+Console.WriteLine(uniffi.dotnet_uniffi.DotnetUniffiMethods.TestPanic("hello"));
+}catch(uniffi.dotnet_uniffi.PanicException ex)
+{
+  Console.Error.WriteLine(ex.Message);
 }
-
---
+++ example-rust-lib/dotnet_uniffi/src/lib.rs
@@ -0,0 +1,42 @@
+use std::time::Duration;
+// You must call this once
+uniffi::setup_scaffolding!();
+
+// What follows is an intentionally ridiculous whirlwind tour of how you'd expose a complex API to UniFFI.
+
+#[derive(Copy, Clone, Debug, PartialEq, uniffi::Record)]
+pub struct ComputationResult {
+    pub value: i64,
+    pub computation_time: Duration,
+}
+
+#[uniffi::export]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[uniffi::export]
+pub fn sub(a: i32, b: i32) -> i32 {
+    a - b
+}
+
+#[uniffi::export]
+pub fn mul(a: i32, b: i32) -> i32 {
+    a * b
+}
+
+#[uniffi::export]
+pub fn test(num: i32) -> i32 {
+    core::add_one(num)
+}
+
+#[uniffi::export]
+pub fn test_str(mut num: String) -> String {
+    num.push('c');
+    num
+}
+
+#[uniffi::export]
+pub fn test_panic(mut num: String) -> String {
+    panic!("lol")
+}
